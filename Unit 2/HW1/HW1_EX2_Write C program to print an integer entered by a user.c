3c3eefe148fcf35797fc0cb76663c09669d75630
Hesham Mohamed <heshamelzeibak9@gmail.com>
2022-12-20 16:42:44 +0200

    updating files organizing

---
+++ Unit 2/HW1/HW1_EX2_Write C program to print an integer entered by a user.c
@@ -0,0 +1,18 @@
+/*
+ * EX2.c
+ *
+ *  Created on: Dec 6, 2022
+ *      Author: hesham mohamed
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int x;
+	printf("Enter an integer: ");
+	fflush(stdout);
+	scanf("%d", &x);
+	printf("You entered: %d\n",x);
+}
